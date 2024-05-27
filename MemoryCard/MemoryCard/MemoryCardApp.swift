726d139134910c02c04a7eb72aae61b011936e74
Gl0ukhov <gmv96@mail.ru>
2024-05-27 14:34:13 +0300

    Добавлен основной экран и экран карты

---
+++ MemoryCard/MemoryCard/MemoryCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardApp.swift
+//  MemoryCard
+//
+//  Created by Матвей Глухов on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
